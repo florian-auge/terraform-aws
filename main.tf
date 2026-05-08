2bd8c1abe26aaf5eba05b2f1ac9867dbe37c716e
Florian Augé <florianauge33@gmail.com>
2026-05-08 15:59:29 +0200

    Ajout rôle IAM, output IP, génération inventory Ansible

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "mon_serveur" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg_florian.id]
   key_name = aws_key_pair.cle_florian.key_name
+  iam_instance_profile   = "ec2-s3-role"
   
   tags = {
     Name = "terraform-florian"
@@ -40,4 +41,11 @@ resource "aws_security_group" "sg_florian" {
 resource "aws_key_pair" "cle_florian" {
   key_name   = "cle-terraform-florian"
   public_key = file("~/.ssh/cle-florian.pub")
+}
+output "ec2_public_ip" {
+  value = aws_instance.mon_serveur.public_ip
+}
+resource "local_file" "ansible_inventory" {
+  content  = "[ec2]\n${aws_instance.mon_serveur.public_ip} ansible_ssh_private_key_file=~/.ssh/cle-florian.pem\n"
+  filename = "../ansible-test/inventory.ini"
 }
